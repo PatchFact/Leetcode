1033d8638f88808ab11258b6099763a223c8f6e9
Patch <patchfact@gmail.com>
2023-12-11 22:44:45 -0800

    formatting

--- problems/217_ContainsDup.py
+++ problems/217_ContainsDup.py
@@ -7,13 +7,12 @@ nums = nums.split(",")
 duplicate = False
 
 for i in range(len(nums)):
-    
     counter = 0
 
     for f in range(len(nums)):
         if nums[i] == nums[f]:
             counter += 1
-    
+
     if counter >= 2:
         duplicate = True
         break
